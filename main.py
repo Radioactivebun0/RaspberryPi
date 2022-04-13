7ed0a8c360d44c701e2ea2f50dd9e738cca0a5fb
Radioactivebun0 <70781995+Radioactivebun0@users.noreply.github.com>
2022-04-13 11:55:12 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,25 @@
+from tkinter import Button
+import RPi.GPIO as GPIO
+import time
+
+button = 40
+
+def setup():
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(button, GPIO.IN)
+
+def loop():
+    while True:
+        if GPIO.input(button) == GPIO.HIGH:
+            print("Button pressed")
+            time.sleep(0.2)
+
+def destroy():
+    GPIO.cleanup()
+
+if __name__ == '__main__':
+    setup()
+    try:
+        loop()
+    except KeyboardInterrupt:
+        destroy()
