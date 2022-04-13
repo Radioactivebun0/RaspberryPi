3f2cd7b3a32e8314b84ef80eb6c697e45e51108a
Radioactivebun0 <70781995+Radioactivebun0@users.noreply.github.com>
2022-04-13 12:28:04 -0700

    Add files via upload

--- main.py
+++ main.py
@@ -4,17 +4,40 @@ import time
 
 button = 4
 
+global lastTime
+lastTime = time.time()
+
 def setup():
     GPIO.setmode(GPIO.BCM)
     GPIO.setup(button, GPIO.IN)
 
+def alert(al):
+    if al == True and time.time() - lastTime > 60:
+        print("Alert!")
+    else:
+        if al == True:
+            print("Alert already sent per lastTime")
+        else:
+            print("No alert at all")
+
 def loop():
+    alert = False
     while True:
+        if GPIO.input(button) == GPIO.HIGH:
+            alert = True
+            print("Alert set to True")
+        else:
+            alert = False
+            print("Alert set to False")
+        
+        time.sleep(5)
+
+        """
         if GPIO.input(button) == GPIO.HIGH:
             print("Button pressed")
         else:
             print("Button released")
-        time.sleep(0.2)
+        time.sleep(0.2)"""
 
 def destroy():
     GPIO.cleanup()
