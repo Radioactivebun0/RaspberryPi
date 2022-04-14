b52dc2341e043effbe8da0ac8f023c0958c064a2
Radioactivebun0 <70781995+Radioactivebun0@users.noreply.github.com>
2022-04-14 12:23:21 -0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from tkinter import Button
 import RPi.GPIO as GPIO
 import time
+import smtplib
+from email.mime.text import MIMEText
 
 button = 4
 
@@ -11,11 +13,30 @@ def setup():
     GPIO.setmode(GPIO.BCM)
     GPIO.setup(button, GPIO.IN)
 
+def send_email(too, sub="test", mm="test"):
+    SMTP_SERVER = "smtp.mail.yahoo.com"
+    SMTP_PORT = 587
+    SMTP_USERNAME = "leak_detection@yahoo.com" #leak_detection@yahoo.com @att.net Soccer0127=
+    SMTP_PASSWORD = "qyfivnxtrequzavv" # qyfivnxtrequzavv
+    EMAIL_FROM = "leak_detection@yahoo.com"
+    EMAIL_TO = too
+    EMAIL_SUBJECT = sub
+    co_msg = mm
+    msg = MIMEText(co_msg)
+    msg['Subject'] = EMAIL_SUBJECT
+    msg['From'] = EMAIL_FROM 
+    msg['To'] = EMAIL_TO
+    mail = smtplib.SMTP(SMTP_SERVER, SMTP_PORT)
+    mail.starttls()
+    mail.login(SMTP_USERNAME, SMTP_PASSWORD)
+    mail.sendmail(EMAIL_FROM, EMAIL_TO, msg.as_string())
+    mail.quit()
+
 def alert(al):
     global lastTime
     if al == "True" and time.time() - lastTime > 60:
         lastTime = time.time()
-        print("Alert!")
+        send_email("tyleer253@gmail.com", "Leak Detected", "Leak Detected")
     else:
         if al == "True":
             print("Alert already sent per lastTime")
@@ -35,13 +56,6 @@ def loop():
         time.sleep(5)
 
         alert(str(alertboo))
-
-        """
-        if GPIO.input(button) == GPIO.HIGH:
-            print("Button pressed")
-        else:
-            print("Button released")
-        time.sleep(0.2)"""
 
 def destroy():
     GPIO.cleanup()
